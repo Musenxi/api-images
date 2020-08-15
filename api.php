515e2ce6b78a077ed1245c1f63882a3c68493839
霂森西 <lh1647224024@outlook.com>
2020-08-15 17:41:08 +0800

    Update api.php

--- api.php
+++ api.php
@@ -1,6 +1,8 @@
 <?php
 
-$rand = rand(1,100);
+$rand = rand(0,15);
 $url = 'https://cdn.jsdelivr.net/gh/Musenxi/homepage/bg/image/'.$rand.'.jpg';
-header("Location:".$url);
+$img = file_get_contents($url,true);
+header("Content-Type: image/jpeg;");
+echo $img;
 ?>
