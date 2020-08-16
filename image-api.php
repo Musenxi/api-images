fce53e263318b03d63f7fb79d75ac46e5a748b7f
霂森西 <lh1647224024@outlook.com>
2020-08-16 16:10:02 +0800

    Update image-api.php

--- image-api.php
+++ image-api.php
@@ -1,6 +1,6 @@
 <?php
 
-$rand = rand(0,15);
+$rand = rand(0,17);
 $url = 'https://cdn.jsdelivr.net/gh/Musenxi/homepage/bg/image/'.$rand.'.jpg';
 $img = file_get_contents($url,true);
 header("Content-Type: image/jpeg;");
